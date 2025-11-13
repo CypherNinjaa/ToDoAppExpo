0890b00d24e77a930923a741166aedbbce20d1f2
vikash <vikashkelly@gmail.com>
2025-11-13 08:20:18 +0530

    chore: add ESLint and Prettier configuration
    - Updated package.json to include ESLint, Prettier, and TypeScript ESLint dependencies.
    - Created .eslintrc.js for ESLint configuration with TypeScript support and Prettier integration.
    - Added .prettierrc.js for Prettier configuration with specified formatting rules.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  env: {
+    'react-native/react-native': true,
+    node: true,
+    es6: true,
+  },
+};
